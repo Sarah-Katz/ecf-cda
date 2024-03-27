100f115ade875c85601d452f382099de11e3d473
Sarah-Katz <sarakatz59@gmail.com>
2024-03-27 09:55:17 +0100

    feat: Added queries to Bed repository

--- api/src/main/java/co/simplon/sarah/api/ecf/persistance/repository/bed/IBedRepository.java
+++ api/src/main/java/co/simplon/sarah/api/ecf/persistance/repository/bed/IBedRepository.java
@@ -1,8 +1,11 @@
 package co.simplon.sarah.api.ecf.persistance.repository.bed;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import co.simplon.sarah.api.ecf.persistance.entity.Bed;
 
 public interface IBedRepository extends JpaRepository<Bed, Integer> {
+    @Query(BedQueries.FIND_BY_PATIENT_ID)
+    Bed findByPatientId(final int idPatient);
 }
