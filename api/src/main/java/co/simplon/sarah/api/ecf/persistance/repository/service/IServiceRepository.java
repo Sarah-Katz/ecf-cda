7ad5f85c455a4a2846eddadb1fa603baff6cfb78
Sarah-Katz <Sarahkatz59@gmail.com>
2024-03-28 19:58:25 +0100

    fix(repositories): Added missing @Repository

--- api/src/main/java/co/simplon/sarah/api/ecf/persistance/repository/service/IServiceRepository.java
+++ api/src/main/java/co/simplon/sarah/api/ecf/persistance/repository/service/IServiceRepository.java
@@ -1,8 +1,10 @@
 package co.simplon.sarah.api.ecf.persistance.repository.service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import co.simplon.sarah.api.ecf.persistance.entity.Service;
 
+@Repository
 public interface IServiceRepository extends JpaRepository<Service, Integer> {
 }
