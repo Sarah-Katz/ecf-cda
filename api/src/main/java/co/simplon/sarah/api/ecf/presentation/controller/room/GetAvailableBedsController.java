7c0694d28ed1366416346754fbe7171bea76ad7f
Sarah-Katz <Sarahkatz59@gmail.com>
2024-03-29 11:39:30 +0100

    feat(Room): Added GetAvailableBedController

---
+++ api/src/main/java/co/simplon/sarah/api/ecf/presentation/controller/room/GetAvailableBedsController.java
@@ -0,0 +1,30 @@
+package co.simplon.sarah.api.ecf.presentation.controller.room;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.sarah.api.ecf.business.dto.BedDto;
+import co.simplon.sarah.api.ecf.business.service.room.IRoomService;
+
+@RestController
+@CrossOrigin
+public class GetAvailableBedsController {
+    private IRoomService roomService;
+
+    public GetAvailableBedsController(final IRoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    /**
+     * @param serviceId The id of the service for which to get the available beds
+     * @return A list of available beds for the given service
+     */
+    @GetMapping("/rooms/beds/{serviceId}")
+    public List<BedDto> getAvailableBeds(@PathVariable final int serviceId) {
+        return roomService.getAvailableBeds(1);
+    }
+}
