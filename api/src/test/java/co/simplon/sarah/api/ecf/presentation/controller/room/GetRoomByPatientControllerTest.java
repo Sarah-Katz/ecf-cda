96cad05ca9500257b934e3db6e1a76079848f573
Sarah-Katz <Sarahkatz59@gmail.com>
2024-03-29 11:44:50 +0100

    feat(Room) Added GetRoomByPatientController

---
+++ api/src/test/java/co/simplon/sarah/api/ecf/presentation/controller/room/GetRoomByPatientControllerTest.java
@@ -0,0 +1,30 @@
+package co.simplon.sarah.api.ecf.presentation.controller.room;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import co.simplon.sarah.api.ecf.business.dto.RoomDto;
+import co.simplon.sarah.api.ecf.business.service.room.IRoomService;
+
+@ExtendWith(MockitoExtension.class)
+class GetRoomByPatientControllerTest {
+    @InjectMocks
+    private GetRoomByPatientController getRoomByPatientController;
+    @Mock
+    private IRoomService roomService;
+
+    @Test
+    void getRoomByPatient() {
+        when(roomService.getRoomByPatient(anyInt())).thenReturn(new RoomDto());
+        getRoomByPatientController.getRoomByPatient(1);
+        verify(roomService, times(1)).getRoomByPatient(anyInt());
+    }
+}
