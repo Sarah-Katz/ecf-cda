c0c94e5b13119f7ff6685a0281b772fe86c4bd23
Sarah-Katz <Sarahkatz59@gmail.com>
2024-03-29 09:49:34 +0100

    feat(Patient): Added UpdatePatient

---
+++ api/src/main/java/co/simplon/sarah/api/ecf/presentation/controller/patient/UpdatePatientController.java
@@ -0,0 +1,27 @@
+package co.simplon.sarah.api.ecf.presentation.controller.patient;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.sarah.api.ecf.business.dto.PatientDto;
+import co.simplon.sarah.api.ecf.business.service.patient.IPatientService;
+
+@RestController
+@CrossOrigin
+public class UpdatePatientController {
+    private IPatientService patientService;
+
+    public UpdatePatientController(IPatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    /**
+     * @param patientDto the patientDto to add to the database
+     */
+    @PutMapping("/patients")
+    public void updatePatient(@RequestBody final PatientDto patientDto) {
+        patientService.savePatient(patientDto);
+    }
+}
