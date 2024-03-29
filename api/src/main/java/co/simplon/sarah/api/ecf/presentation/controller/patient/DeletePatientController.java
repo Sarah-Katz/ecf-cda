a53117f2c93ae66c5d129409503bf7e964cceb6f
Sarah-Katz <Sarahkatz59@gmail.com>
2024-03-29 10:38:15 +0100

    feat(Patient): Added DeletePatientController

---
+++ api/src/main/java/co/simplon/sarah/api/ecf/presentation/controller/patient/DeletePatientController.java
@@ -0,0 +1,26 @@
+package co.simplon.sarah.api.ecf.presentation.controller.patient;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.sarah.api.ecf.business.service.patient.IPatientService;
+
+@RestController
+@CrossOrigin
+public class DeletePatientController {
+    private IPatientService patientService;
+
+    public DeletePatientController(final IPatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    /**
+     * @param id the id of the patient to delete
+     */
+    @DeleteMapping("/users/$id")
+    public void deletePatientById(@RequestParam final int id) {
+        patientService.deletePatient(id);
+    }
+}
