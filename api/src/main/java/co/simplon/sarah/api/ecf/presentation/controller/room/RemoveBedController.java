f488d00d37cff530c9bfbaacc679593e08968b65
Sarah-Katz <Sarahkatz59@gmail.com>
2024-03-29 12:01:06 +0100

    feat(Room): Added RemoveBedController

--- api/src/main/java/co/simplon/sarah/api/ecf/presentation/controller/room/RemoveBedController.java
+++ api/src/main/java/co/simplon/sarah/api/ecf/presentation/controller/room/RemoveBedController.java
@@ -6,24 +6,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.simplon.sarah.api.ecf.business.dto.BedDto;
-import co.simplon.sarah.api.ecf.business.dto.RoomDto;
 import co.simplon.sarah.api.ecf.business.service.room.IRoomService;
 
 @RestController
 @CrossOrigin
-public class AddBedController {
+public class RemoveBedController {
     private IRoomService roomService;
 
-    public AddBedController(final IRoomService roomService) {
+    public RemoveBedController(final IRoomService roomService) {
         this.roomService = roomService;
     }
 
     /**
-     * @param room The room to add the bed to
-     * @param bed  The bed to add to the room
+     * @param bed The bed to remove from the room
      */
-    @PostMapping("/rooms/addBed")
-    public void addBed(@RequestBody final RoomDto room, @RequestBody final BedDto bed) {
-        roomService.addBed(room, bed);
+    @PostMapping("/rooms/removeBed")
+    public void removeBed(@RequestBody final BedDto bed) {
+        roomService.removeBed(bed);
     }
 }
