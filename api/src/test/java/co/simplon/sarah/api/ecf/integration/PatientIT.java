c07460dc8f260f701c161de8839f5e3010a60c00
Sarah-Katz <Sarahkatz59@gmail.com>
2024-04-12 20:21:59 +0200

    build(ci): added build and deploy action

--- api/src/test/java/co/simplon/sarah/api/ecf/integration/PatientIT.java
+++ api/src/test/java/co/simplon/sarah/api/ecf/integration/PatientIT.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Date;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
